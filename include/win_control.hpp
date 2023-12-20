516d397ea02dc887e465e9ece43dd4423f907f50
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-12-20 09:01:17 +0100

    Up

--- include/win_control.hpp
+++ include/win_control.hpp
@@ -6,6 +6,7 @@
 
 namespace win_control
 {
+  extern node *m_node_ptr;
   void exec_cmd(std::string cmd);
   void init();
   void run();
--- source/win_control/init.cpp
+++ source/win_control/init.cpp
@@ -1,5 +1,8 @@
 #include <win_control.hpp>
 
+node *win_control::m_node_ptr;
+
 void win_control::init()
 {
+  m_node_ptr = g_data->tree;
 }
