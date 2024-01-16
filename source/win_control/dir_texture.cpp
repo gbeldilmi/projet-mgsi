62a362b98a87d224493bf0136cd8141a2a8af917
smqdes <theo.corbel21@gmail.com>
2024-01-16 21:19:53 +0100

    CLI finie

--- source/win_control/dir_texture.cpp
+++ source/win_control/dir_texture.cpp
@@ -8,17 +8,22 @@ void win_control::dir_texture(std::vector<std::string> arguments)
   }
   else
   {
-    if(arguments[0] == "normal") //applique une texture normal map
+    if ((!arguments[1].isInt())) //si le deuxième argument n'est pas un entier
     {
-      
+      std::cout << "Erreur : le deuxième argument doit être un entier" << std::endl; //affiche une erreur
+      std::cout << "Tapez \u001B[34mhelp texture\u001B[0m pour afficher l'aide" << std::endl; //affiche l'aide
+    }
+    else if(arguments[0] == "normal") //applique une texture normal map
+    {
+      m_current_node->set_maps(arguments[1].toInt(), 0, 0); //applique la texture
     }
     else if(arguments[0] == "displacement") //applique une texture displacement map
     {
-
+      m_current_node->set_maps(0, arguments[1].toInt(), 0); //applique la texture
     }
     else if(arguments[0] == "color") //applique une texture color map
     {
-
+      m_current_node->set_maps(0, 0, arguments[1].toInt()); //applique la texture
     }
     else
     {
