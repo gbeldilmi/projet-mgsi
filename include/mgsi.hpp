bdbaa54c3213849fa6c9e1bed1c740ce97166b17
Guillaume BELDILMI <gbeldilmi@pm.me>
2024-01-16 17:22:19 +0100

    Up

--- include/mgsi.hpp
+++ include/mgsi.hpp
@@ -2,4 +2,5 @@
 
 #include <data_model.hpp>
 
+// global data shared around the program
 extern data_model_t *g_data;
--- include/win_control.hpp
+++ include/win_control.hpp
@@ -8,14 +8,16 @@
 
 namespace win_control
 {
-  extern node *m_current;
+  extern node *m_current; // current node selected (used to navigate inside the tree)
   // list of directives
   void dir_help(std::vector<std::string> arguments);
   void dir_list(std::vector<std::string> arguments);
   void dir_node(std::vector<std::string> arguments);
-  // other functions
+  // execute command from a directive and its arguments
   void exec_cmd(std::string, std::vector<std::string>);
+  // extract directive and its arguments from a string
   void extract_directive(std::string, std::string &, std::vector<std::string> &);
+  // initialize and run
   void init();
   void run();
 };
