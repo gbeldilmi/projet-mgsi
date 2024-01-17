1fd7074deec159f70528640e3913438efb07240c
ElioGence <genceelio@gmail.com>
2024-01-17 15:47:58 +0100

    dernieres modifs

--- source/win_control/dir_texture.cpp
+++ source/win_control/dir_texture.cpp
@@ -2,28 +2,28 @@
 
 void win_control::dir_texture(std::vector<std::string> arguments)
 {
-  if ((arguments.size() < 3) || (arguments.size() > 3)) //si le nombre d'arguments est incorrect
+  if ((arguments.size() < 2) || (arguments.size() > 2)) //si le nombre d'arguments est incorrect
   {
     dir_help(std::vector<std::string>({"texture"})); //affiche l'aide
   }
   else
   {
-    if ((stoi(arguments[2]) == NULL) || (stoi(arguments[2]) <= 0)) //si le deuxième argument n'est pas un entier supérieur à 0
+    if ((stoi(arguments[1]) == NULL) || (stoi(arguments[1]) <= 0)) //si le deuxième argument n'est pas un entier supérieur à 0
     {
       std::cout << "Erreur : le deuxième argument doit être un entier supérieur à 0" << std::endl; //affiche une erreur
       std::cout << "Tapez \u001B[34mhelp texture\u001B[0m pour afficher l'aide" << std::endl; //affiche l'aide
     }
-    else if(arguments[1] == "normal") //applique une texture normal map
+    else if(arguments[0] == "normal") //applique une texture normal map
     {
-      m_current->set_maps(stoi(arguments[2]), 0, 0); //applique la texture
+      m_current->set_maps(stoi(arguments[1]), 1, 1); //applique la texture
     }
-    else if(arguments[1] == "displacement") //applique une texture displacement map
+    else if(arguments[0] == "displacement") //applique une texture displacement map
     {
-      m_current->set_maps(0, stoi(arguments[2]), 0); //applique la texture
+      m_current->set_maps(1, stoi(arguments[1]), 1); //applique la texture
     }
-    else if(arguments[1] == "color") //applique une texture color map
+    else if(arguments[0] == "color") //applique une texture color map
     {
-      m_current->set_maps(0, 0, stoi(arguments[2])); //applique la texture
+      m_current->set_maps(1, 1, stoi(arguments[1])); //applique la texture
     }
     else
     {
--- source/win_view/draw_sphere.cpp
+++ source/win_view/draw_sphere.cpp
@@ -2,43 +2,34 @@
 
 void win_view::draw_sphere(int maps)
 {
-  int c = extract_color_map(maps),
-      n = extract_normal_map(maps),
-      d = extract_displacement_map(maps),
-      i, j, slices = 20, stacks = 20;
-  float x, y, z;
-  glBegin(GL_TRIANGLE_FAN);
-  glVertex3f(0, 0.5, 0);
-  for (i = 0; i <= slices; ++i)
-  {
-    x = cosf((2 * M_PI * i) / slices);
-    y = sinf((2 * M_PI * i) / slices);
-    glVertex3f(x, y, 0.5);
-  }
-  glEnd();
-  glBegin(GL_TRIANGLE_STRIP);
-  for (i = 0; i < stacks; ++i)
-  {
-    for (j = 0; j <= slices; ++j)
+    int c = extract_color_map(maps),
+        n = extract_normal_map(maps),
+        d = extract_displacement_map(maps),
+        i, j, slices = 20, stacks = 20;
+    float x, y, z, theta, phi, radius = 0.5;
+
+    for (i = 0; i < stacks; ++i)
     {
-      x = cosf((2 * M_PI * j) / slices) * sinf((M_PI * (i + 1)) / stacks);
-      y = sinf((2 * M_PI * j) / slices) * sinf((M_PI * (i + 1)) / stacks);
-      z = cosf((M_PI * (i + 1)) / stacks);
-      glVertex3f(x, y, z);
-      x = cosf((2 * M_PI * j) / slices) * sinf((M_PI * i) / stacks);
-      y = sinf((2 * M_PI * j) / slices) * sinf((M_PI * i) / stacks);
-      z = cosf((M_PI * i) / stacks);
-      glVertex3f(x, y, z);
+        glBegin(GL_TRIANGLE_STRIP);
+        for (j = 0; j <= slices; ++j)
+        {
+            theta = (2.0 * M_PI * j) / slices;
+            phi = (M_PI * (i + 0.5)) / stacks;
+
+            x = radius * sin(phi) * cos(theta);
+            y = radius * cos(phi);
+            z = radius * sin(phi) * sin(theta);
+
+            glVertex3f(x, y, z);
+
+            theta = (2.0 * M_PI * (j + 1)) / slices;
+
+            x = radius * sin(phi) * cos(theta);
+            y = radius * cos(phi);
+            z = radius * sin(phi) * sin(theta);
+
+            glVertex3f(x, y, z);
+        }
+        glEnd();
     }
-  }
-  glEnd();
-  glBegin(GL_TRIANGLE_FAN);
-  glVertex3f(0, -0.5, 0);
-  for (i = 0; i <= slices; ++i)
-  {
-    x = cosf((2 * M_PI * i) / slices);
-    y = sinf((2 * M_PI * i) / slices);
-    glVertex3f(x, y, -0.5);
-  }
-  glEnd();
 }
